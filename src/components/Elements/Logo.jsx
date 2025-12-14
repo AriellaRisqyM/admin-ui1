ca8342e9ffac17c0089c6537bf2847f310b5bcc9
AriellaRisqyM <ariellarisqy@gmail.com>
2025-12-14 18:15:43 +0700

    atomic design page sign in

---
+++ src/components/Elements/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
+          <span className="font-bold">Fine</span>bank<span className="font-bold">.IO</span>
+    </div>
+  );
+}
+
+export default Logo
